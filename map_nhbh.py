81c8d1c66c4fff2757b84f2476ca2a2a4f69ce7f
Yesol Lee <yesolee@kakao.com>
2024-08-15 16:29:48 +0900

    map 수정

--- map_nhbh.py
+++ map_nhbh.py
@@ -18,7 +18,7 @@ def creat_map(color_list, color_list_name):
     sorted_neighborhoods = sorted(df['Neighborhood'].unique())
 
     nhbh_individuals = {i: [] for i in df['Neighborhood']}
-    nhbh_color = {nhbh: color for nhbh, color in zip(nhbh_individuals.keys(), palette_nhbh)}
+    nhbh_color = {nhbh: color for nhbh, color in zip(sorted_neighborhoods, palette_nhbh)}
 
     # 각 동네의 점들 그룹화
     for nhbh, lat, lon in zip(df['Neighborhood'], df['Latitude'], df['Longitude']):
@@ -113,79 +113,62 @@ creat_map(all_gray,"all_gray")
 df.columns
 exteriors = set(df['Exterior_1st'].unique()) | set(df['Exterior_2nd'].unique())
 len(exteriors)
-
+Exterior_all = pd.DataFrame()
+Exterior_all['Exterior'] = sorted(list(exteriors))
 first = df.groupby(['Exterior_1st'],as_index=False).agg(first = ('Exterior_1st','count'))
 second = df.groupby(['Exterior_2nd'],as_index=False).agg(second = ('Exterior_2nd','count'))
+first.columns = ['Exterior','Exterior_1st']
+second.columns = ['Exterior','Exterior_2nd']
+Exterior_all
 
-Exterior_all = pd.DataFrame()
-Exterior_all
-Exterior_all['Exterior'] = sorted(list(exteriors))
-Exterior_all
 # left join 수행
-Exterior = pd.merge(Exterior_all, first, on='key', how='left')
+Exterior_all = pd.merge(Exterior_all, first, on='Exterior', how='left')
+Exterior_all = pd.merge(Exterior_all, second, on='Exterior', how='left')
+Exterior_all = Exterior_all.sort_values(['Exterior_1st','Exterior_1st'],ascending=False) 
+Exterior_all.reset_index(drop=True, inplace=True)
+Exterior_all.head(5)
 
-# NaN 값을 0으로 채우기
-result = result.fillna(0)
-
-print(result)
-
-
-
-
-
-
-
-
-
-
-first
-first = first.sort_values('Exterior_1st')
-first.reset_index(drop=True, inplace=True)
-first.columns = ['Exterior','first']
-second.columns = ['Exterior','second']
-second
-Exterior = pd.merge(first,second,on='Exterior')
-Exterior = Exterior.sort_values('first',ascending=False)
-Exterior.reset_index(drop=True, inplace=True)
-Exterior.sum()
+palette_exterior = ["#FF6F61","#6F9FD8","#F7C94C","#6DBE45","#FF8C00"] + ["#D3D3D3"]*11
+exterior = Exterior_all.dropna(subset=['Exterior_1st'])['Exterior']
+exteriors_color = {ext: color for ext, color in zip(exterior, palette_exterior)}
+exteriors_color
 
 fig = go.Figure()
 
-fig.add_trace(go.Scattermapbox(
-                lat=points[:, 0],
-                lon=points[:, 1],
-                mode='lines+markers',
-                line=dict(color=nhbh_color[nhbh]),
-                hovertext=nhbh,
-                hoverinfo='text',
-                name=nhbh
-            ))
+for ext in exterior:
+    filtered_df = df[df['Exterior_1st'] == ext]
+    fig.add_trace(go.Scattermapbox(
+        lat=filtered_df['Latitude'],
+        lon=filtered_df['Longitude'],
+        mode='markers',
+        marker=dict(color=exteriors_color[ext]),
+        hovertext=filtered_df['Exterior_1st'],
+        hoverinfo='text',
+        name=ext,  # 각 트레이스에 고유한 이름을 부여
+        showlegend=True  # 범례 표시
+    ))
 
-palette_exterior_top5 = 
+# 레이아웃 업데이트
+fig.update_layout(
+    mapbox_style="carto-positron",
+    mapbox=dict(
+        center=go.layout.mapbox.Center(lat=42.034482, lon=-93.642897),
+        zoom=12
+    ),
+    margin={"r":0,"t":0,"l":0,"b":0},
+    legend=dict(
+        font=dict(
+            size=10  # 범례 텍스트 크기를 10으로 설정
+        )
+    ),
+    legend_title=dict(text='Exterior 1st',
+                        font=dict(
+                            size=12,  # 제목의 크기 설정
+                            color="black",  # 제목의 색상 설정
+                            family="Arial",  # 폰트 설정
+                            weight="bold"  # 굵게 표시
+    )),
+)
 
-Exterior
-colors = 
-
-
-{"VinylSd" : }
-[
-'#FF6F61',  # VinylSd
-'#6F9FD8',  # MetalSd
-'#F7C94C',  # HdBoard
-'#6DBE45',  # Wd Sdng
-'#FF8C00',  # Plywood
-'#4DB6AC',  # CemntBd
-'#9B59B6',  # BrkFace
-] + ['#D3D3D3'] * (len(exterior_distribution['Exterior_1st'].unique()) - 7)  # 나머지 색상은 회색으로 설정
-
-
-
-
-
-
-
-코랄 핑크 (Coral Pink): #FF6F61
-페일 옐로우 (Pale Yellow): #FFD662
-소프트 블루 (Soft Blue): #92A8D1
-민트 그린 (Mint Green): #88B04B
-라벤더 퍼플 (Lavender Purple): #B565A
+fig.write_html("map_exterior.html")
+webbrowser.open_new("map_exterior.html")
